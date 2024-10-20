1d4ed265f6b9cee6c66d75450d20009a1ffd73b1
Clara Torres <clara.torres01@estudiant.upf.edu>
2024-10-20 11:34:50 +0200

    Quart intent

--- src/antiprime.py
+++ src/antiprime.py
@@ -3,55 +3,62 @@
 ## COMMAND LINE SPECIFIED BELOW
 
 import sys
-x = int(sys.argv[1]) if len(sys.argv) > 1 else 0
 
 
-def main(x) :
-	## YOU CODE SHOULD START HERE AST THE SAME
-	## IDENTATION AS THIS COMMENT
+# Comprovar que hi ha almenys un argument i que és un número enter
+if len(sys.argv) > 1 and sys.argv[1].isdigit():
+    x = int(sys.argv[1])
+   
+	def main(x) :
+		## YOU CODE SHOULD START HERE AST THE SAME
+		## IDENTATION AS THIS COMMENT
 
-	i = 1
-	sx = 0
-	s = 0
-	j = 1
-
-	# Comptem els divisors del número donat
-	while (i <= x):
-		if (x % i == 0):
-			sx = sx + 1
-		i = i + 1	
-
-	print(sx)
-
-	# Comprovem els divisors dels números més petits
-	while (x - 1 > 0):
-		x = x - 1
+		i = 1
+		sx = 0
 		s = 0
-		j = 1 
-		while (j <= x):
-			if (x % j == 0):
-				s = s + 1
-			j = j + 1
-		print(s)
+		j = 1
 
-		if (s >= sx):
-			return("not anti-prime")
-	else:
-		return("anti-prime")
+		# Comptem els divisors del número donat
+		while (i <= x):
+			if (x % i == 0):
+				sx = sx + 1
+			i = i + 1	
 
-	## THE LAST LINES OF YOUR CODE SHOULD EITHER
-	## RETURN THE VALUE "anti-prime" or "not anti-prime"
-	## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
-	## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
-	## "anti-prime" or "not anti-prime"
+		print(sx)
 
-## DO NOT REMOVE THIS LINE BELOW
-if __name__ == "__main__" :
+		# Comprovem els divisors dels números més petits
+		while (x - 1 > 0):
+			x = x - 1
+			s = 0
+			j = 1 
+			while (j <= x):
+				if (x % j == 0):
+					s = s + 1
+				j = j + 1
+			print(s)
 
-	## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
-	## TO RUN THIS PROGRAM AS, FOR INSTANCE:
-	## $ python antiprime.py 6
-	## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
-	## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
+			if (s >= sx):
+				return("not anti-prime")
+		else:
+			return("anti-prime")
 
-	print(main(x))
+
+		## THE LAST LINES OF YOUR CODE SHOULD EITHER
+		## RETURN THE VALUE "anti-prime" or "not anti-prime"
+		## REPLACE THE FOLLOWING LINE BY WHATEVER LINES
+		## OF CODE ALLOW THIS FUNCTION TO RETURN THE VALUE
+		## "anti-prime" or "not anti-prime"
+
+	## DO NOT REMOVE THIS LINE BELOW
+	if __name__ == "__main__" :
+
+		## MODIFY THE LINE BELOW AND ADD BEFORE WHATEVER LINES ARE NECESSARY
+		## TO RUN THIS PROGRAM AS, FOR INSTANCE:
+		## $ python antiprime.py 6
+		## WHERE THE FIRST ARGUMENT IS A POSITIVE INTEGER NUMBER FOR WHICH
+		## YOU WANT TO FIGURE OUT WHETHER IS ANTI-PRIME OR NOT
+
+		print(main(x))
+
+else:
+    print("Error: Please provide a valid integer.")
